8581ae698071d94003b89d20fc87e0395173a5d3
Yiqun <yiqun_rong@trendmicro.com>
2023-06-23 22:15:15 +1000

    fix: video file download

--- src/main/src/pages/Popup/index.tsx
+++ src/main/src/pages/Popup/index.tsx
@@ -1,16 +1,42 @@
-import { useState, useEffect, useCallback, useRef } from 'react';
-import { Button } from '@douyinfe/semi-ui';
-import { IconDownloadStroked, IconSettingStroked } from '@douyinfe/semi-icons';
+import { useState, useEffect, useCallback, useRef } from "react";
+import { Button } from "@douyinfe/semi-ui";
+import { IconDownloadStroked, IconSettingStroked } from "@douyinfe/semi-icons";
 
-import { Video } from '../../../../base/functions/downloadVideo';
+import { Video } from "../../../../base/functions/downloadVideo";
+
+function downloadVideoFile(filename: string, url: string) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+  return new Promise((resolve, reject) => {
+    xhr.onload = function () {
+      var urlCreator = window.URL || window.webkitURL;
+      var fileUrl = urlCreator.createObjectURL(this.response);
+      var tag = document.createElement("a");
+      tag.href = fileUrl;
+      tag.target = "_blank";
+      tag.download = filename;
+      document.body.appendChild(tag);
+      tag.click();
+      document.body.removeChild(tag);
+      resolve({});
+    };
+    xhr.onerror = (err) => {
+      alert("Failed to download video file");
+      reject({});
+    };
+    xhr.send();
+  });
+}
 
 export default function Popup() {
   const [video, setVideo] = useState<Video>();
+  const [videoDownloading, setVideoDownloading] = useState(false);
   const port = useRef<chrome.runtime.Port>();
 
   useEffect(() => {
     chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-      port.current = chrome.tabs.connect(tabs[0].id || 0, { name: 'popup' });
+      port.current = chrome.tabs.connect(tabs[0].id || 0, { name: "popup" });
       port.current.onMessage.addListener((msg) => {
         setVideo(msg.video);
       });
@@ -20,24 +46,13 @@ export default function Popup() {
     };
   }, []);
 
-  const downloadVideo = useCallback(() => {
+  const downloadVideo = useCallback(async () => {
     if (!video) {
       return;
     }
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=579563
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1288041
-    chrome.downloads.download(
-      {
-        url: video.url,
-        filename: video.filename,
-        saveAs: true,
-      },
-      () => {
-        setTimeout(() => {
-          window.close();
-        }, 2000);
-      }
-    );
+    setVideoDownloading(true);
+    await downloadVideoFile(video.filename, video.url);
+    setVideoDownloading(false);
   }, [video]);
 
   return (
@@ -49,7 +64,8 @@ export default function Popup() {
         disabled={!video}
         onClick={() => downloadVideo()}
       >
-        {video ? '下载当前视频' : '未检测到视频'}
+        {videoDownloading && "下载中..."}
+        {!videoDownloading && (video ? "下载当前视频" : "未检测到视频")}
       </Button>
       <Button
         icon={<IconSettingStroked />}
@@ -64,11 +80,11 @@ export default function Popup() {
 }
 
 async function openOptionsPage() {
-  const baseUrl = chrome.runtime.getURL('main/index.html');
-  const url = baseUrl + '#/options';
+  const baseUrl = chrome.runtime.getURL("main/index.html");
+  const url = baseUrl + "#/options";
   const tabs = await chrome.tabs.query({
     windowId: chrome.windows.WINDOW_ID_CURRENT,
-    url: baseUrl + '*',
+    url: baseUrl + "*",
   });
   for (const tab of tabs) {
     if (tab.url?.includes(url)) {
